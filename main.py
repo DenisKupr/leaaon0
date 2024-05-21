dae6404915573096f16f7b4252db51f6a7b036eb
DenisKupr <zaq.den@gmail.com>
2024-05-21 17:42:43 +0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,36 @@
+# Задача «Длина слова».
+a="Привет!"
+print((a),len(a))
+
+#Задача «Суммы и разности».
+first=5
+second=2
+
+summ=first+second
+diff=first-second
+
+print(summ)
+print(diff)
+
+#Задача «Среднее арифметическогое».
+first=5
+second=8
+third=10
+
+mean=(first+second+third)/3
+print(mean)
+
+#Задача «Простые строчки».
+first_string="Вторник"
+second_string="Понедельник"
+
+print(second_string,", ",first_string)
+
+#Задача «Сложная формула».
+a=2
+b=6
+c=3
+
+f=(((a*b)+(a*c))**3)/2
+
+print(f)
